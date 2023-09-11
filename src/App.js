ad708bbd90966bdfd9425324a5072874a05a78db
jayurl <j012.patel@gmail.com>
2023-09-11 18:06:05 -0400

    w

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
